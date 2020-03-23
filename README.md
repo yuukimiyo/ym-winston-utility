178cb3e630d76ccafc6c725cb59b98163f993ead
yuukimiyo <yuuki.miyo@gmail.com>
2020-03-23 17:56:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ym-winston-utility
+utility for winston logger.
+
+Winstonのwrapperライブラリです。
+複数のトランスポートを含むロガーの初期化を簡略化する目的で作成しました。
+具体的な利用方法は./sample/index.jsを参照してください。
---
+++ index.js
@@ -0,0 +1,4 @@
+exports.winston = require("./lib/winston");
+exports.ConsoleTransport = require("./lib/ConsoleTransport");
+exports.GCPTransport = require("./lib/GCPTransport");
+exports.SlackTransport = require("./lib/SlackTransport");
